61ec6c45e0f4e128da79b5ea7c73de0d65a85fc2
Egr044 <vulpeeva03@gmail.com>
2026-04-23 10:25:43 +0300

    add system notificare + api error

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -5,4 +5,5 @@ export { photographersApi } from "./photographers";
 export { chatApi } from "./chat";
 export { portfolioApi, packagesApi, testimonialsApi, faqApi } from "./content";
 export { contactApi } from "./contact";
+export { useApiHandler } from "./useApi";
 export type * from "./types";
